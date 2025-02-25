52cb0ca6e5f2a567d077ba8710787d03c1997e93
Mr.Dani <jcosme@gmail.com>
2025-02-25 16:11:30 -0400

    Alfa

--- Datos/DAgendaDeContactos.cs
+++ Datos/DAgendaDeContactos.cs
@@ -13,41 +13,37 @@ namespace Datos
     {
         public int IAgendaDeContactos(EAgendaDeContactos eAgendaDeContactos)
         {
-            string IQuery = "INSERT INTO agenda_de_contactos (NOMBRE,NUMERO) VALUES (@nombre,@numero); SELECT last_insert_id();";
-
             using (MySqlConnection conexion = GetCon())
             {
                 int idAgenda = 0;
-                MySqlCommand cmd = new MySqlCommand(IQuery, conexion);
-                cmd.Parameters.Add("@nombre", MySqlDbType.VarChar).Value = eAgendaDeContactos.nombre;
-                cmd.Parameters.Add("@numero", MySqlDbType.VarChar).Value = eAgendaDeContactos.numero;
+                MySqlCommand cmd = new MySqlCommand("sp_InsertarAgendaDeContactos", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@prm_nombre", MySqlDbType.VarChar).Value = eAgendaDeContactos.nombre;
+                cmd.Parameters.Add("@prm_numero", MySqlDbType.VarChar).Value = eAgendaDeContactos.numero;
                 conexion.Open();
-                int.TryParse(cmd.ExecuteScalar().ToString(), out idAgenda);
-
+                idAgenda = Convert.ToInt32(cmd.ExecuteScalar());  
                 return idAgenda;
             }
         }
 
         public DataTable SListaAgendaDeContactos()
         {
-            string query = "SELECT * FROM agenda_de_contactos;";
             DataTable dt = new DataTable();
-            MySqlDataAdapter adapter = new MySqlDataAdapter(query, GetCon());
+            MySqlDataAdapter adapter = new MySqlDataAdapter("sp_ObtenerAgendaDeContactos", GetCon());
+            adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
             adapter.Fill(dt);
             return dt;
         }
 
         public void UAgendaDeContactos(EAgendaDeContactos eAgendaDeContactos)
         {
-            string query = "UPDATE agenda_de_contactos SET NOMBRE = @nombre, NUMERO = @numero WHERE id_agenda_de_contactos = @id;";
-
             using (MySqlConnection conexion = GetCon())
             {
-                MySqlCommand cmd = new MySqlCommand(query, conexion);
-                cmd.Parameters.Add("@nombre", MySqlDbType.VarChar).Value = eAgendaDeContactos.nombre;
-                cmd.Parameters.Add("@numero", MySqlDbType.VarChar).Value = eAgendaDeContactos.numero;
-                cmd.Parameters.Add("@id", MySqlDbType.Int32).Value = eAgendaDeContactos.id_agenda_de_contactos;
-
+                MySqlCommand cmd = new MySqlCommand("sp_ActualizarAgendaDeContactos", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("prm_nombre", MySqlDbType.VarChar).Value = eAgendaDeContactos.nombre;
+                cmd.Parameters.Add("prm_numero", MySqlDbType.VarChar).Value = eAgendaDeContactos.numero;
+                cmd.Parameters.Add("prm_id_agenda_de_contactos", MySqlDbType.Int32).Value = eAgendaDeContactos.id_agenda_de_contactos;
                 conexion.Open();
                 cmd.ExecuteNonQuery();
             }
@@ -55,38 +51,28 @@ namespace Datos
 
         public DataTable SAgendaDeContactos(int idAgendaDeContactos)
         {
-            string query = "SELECT * FROM agenda_de_contactos WHERE id_agenda_de_contactos = @prm_id_agenda_de_contactos;";
+            
             DataTable dt = new DataTable();
-            MySqlDataAdapter adapter = new MySqlDataAdapter(query, GetCon());
+            MySqlDataAdapter adapter = new MySqlDataAdapter("Buscar_id", GetCon());
+            adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
             adapter.SelectCommand.Parameters.Add("@prm_id_agenda_de_contactos", MySqlDbType.Int32).Value = idAgendaDeContactos;
+            adapter.Fill(dt);
+            return dt;
+
+        }
+
+
+        public DataTable SListaAgenda_de_contactosBusqueda(string query)
+        {
+            DataTable dt = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter("SListaAgenda_de_contactosBusqueda", GetCon());
+            adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+            adapter.SelectCommand.Parameters.Add("query", MySqlDbType.Text).Value = query;
             adapter.Fill(dt);
             return dt;
         }
 
-        public DataTable EjecutarBusqueda(string query)
-        {
-            // Crear la conexión a la base de datos
-            using (MySqlConnection conexion = GetCon())
-            {
-                // Abrir la conexión
-                conexion.Open();
-
-                // Crear el comando para ejecutar el procedimiento almacenado
-                MySqlCommand cmd = new MySqlCommand("Busqueda_De_Contactos", conexion);
-                cmd.CommandType = CommandType.StoredProcedure;
-
-                // Agregar el parámetro 'query' al procedimiento
-                cmd.Parameters.AddWithValue("query", query);
-
-                // Crear un DataAdapter para llenar el DataTable
-                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
-                DataTable dt = new DataTable();
-                da.Fill(dt);
-                return dt;
-
-
-            }
-        }
+        
 
     }
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+# Sistecsoft-Practicas
